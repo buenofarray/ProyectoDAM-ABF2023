1c396cc8ddc67d46ed1df0b7f4b8896268b0cc63
Alberto Bueno <buenofarray@gmail.com>
2023-11-21 05:31:53 +0100

    Configurado Entity Framework, SQLite. Eliminado Swagger, y eliminados temporalmente los requisitos HTTPS.

---
+++ DatabaseHostedService.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ProyectoDAM_ABF2023
+{
+    public class DatabaseHostedService : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.Write("Meow");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
