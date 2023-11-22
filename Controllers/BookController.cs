76217df75a92d0fb7f38eab92530e7d0b0613053
Alberto Bueno <buenofarray@gmail.com>
2023-11-22 10:42:20 +0100

    Final del dia

---
+++ Controllers/BookController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoDAM_ABF2023.Data;
+
+namespace ProyectoDAM_ABF2023.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public BookController(DataContext context) 
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Book>>> AddBook(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetAllBooks()
+        {
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return BadRequest("Character not found.");
+            } return Ok(book);
+        }
+    }
+}
